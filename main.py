8c98b8f02819cb067216f2110677bfc51247d4e0
Cody Grande <cdgrande@cox.net>
2019-12-27 21:52:59 -0700

    improved input checks.

--- main.py
+++ main.py
@@ -6,26 +6,37 @@
 
 tempChoice = input("Please type c to convert from Celsius or f to convert from Fahrenheit.\n")
 endTemp = 0 #so program knows endTemp is an int.
-testTemp = 0 #I needed a int for a workarround sooooo...
+testTemp = 0 
 startTemp = 0
+inputLoop = 0
 
 while tempChoice not in 'cCfF': # Simple user input sanitation
-    print("wrong input detected") # If user hits enter without any input the program crashes. Need to fix.
+    print("wrong input detected") 
     tempChoice = input("Please type c to convert from Celsius or f to convert from Fahrenheit.\n")
 
 #This will let us repeat there start temperature type (F or C)  when getting user input for startTemp
-if (tempChoice =='c') or (tempChoice == 'C'):
-    tempType = 'Celsius'
-else: # Gotta love else, it lets us be a bit lazy.
-    tempType = 'Fahrenheit'
-
-#Gets temperature to convert from as well as sanitizes the user's input
-startTemp = input("Please enter the Temperature in {}.\n".format(tempType))
-while testTemp != 1:
-    if startTemp.isdigit():
-        testTemp = 1
+while inputLoop != 1:
+    if (tempChoice =='c') or (tempChoice == 'C'):
+        tempType = 'Celsius'
+        inputLoop = 1
+    elif (tempChoice =='f') or (tempChoice == 'F'): 
+        tempType = 'Fahrenheit'
+        inputLoop = 1
     else:
-        startTemp = input("Please enter the Temperature in {}.\n".format(tempType))
+        tempChoice = input("Please type c to convert from Celsius or f to convert from Fahrenheit.\n")
+
+
+while testTemp != 1:
+    try:
+        startTemp = float(input("Please enter the Temperature in {}.\n".format(tempType)))
+    except ValueError:
+        print ("Please use a number.")
+    else:
+        break
+#    if startTemp.isnumeric(): 
+#        testTemp = 1
+#    else:
+#        startTemp = input("Please enter the Temperature in {}.\n".format(tempType))
         
 
 # C = (F - 32) / 1.8
